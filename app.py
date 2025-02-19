fca45333a45f2a7e3ebaf9e7e5e7839238dcb35e
hrysdev <hrysdev@gmail.com>
2025-02-19 13:58:32 +0900

    [feat] クリアボタンとopencvのパッケージ周りを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from uuid import uuid4
 
 import streamlit as st
 from streamlit import session_state as ss
@@ -10,6 +11,18 @@ from modules import (
     convert_video_to_image,
     is_image_size_matched,
 )
+
+
+def display_gallery(
+    images: list[UploadedFile | BytesIO], similarities: list[float]
+) -> None:
+    num_cols = 3
+    image_cols = st.columns(num_cols, gap="medium")
+    for i in range(len(images)):
+        with image_cols[i % num_cols]:
+            caption = images[i].name[:15] + "..."
+            st.image(images[i], caption=caption, use_container_width=True)
+            st.html(f"<center>{similarities[i]}</center>")
 
 
 def display_sort_selectbox(
@@ -38,38 +51,34 @@ def display_sort_selectbox(
         display_gallery(images, similarities)
 
 
-def display_gallery(
-    images: list[UploadedFile | BytesIO], similarities: list[float]
-) -> None:
-    num_cols = 3
-    image_cols = st.columns(num_cols, gap="medium")
-    for i in range(len(images)):
-        with image_cols[i % num_cols]:
-            caption = images[i].name[:15] + "..."
-            st.image(images[i], caption=caption, use_container_width=True)
-            st.html(f"<center>{similarities[i]}</center>")
-
-
 def init() -> None:
-    if "similarities" not in ss:
-        ss.similarities = []
+    if "old_image_clear_key" not in ss:
+        ss.old_image_clear_key = str(uuid4())
 
     if "old_image" not in ss:
         ss.old_image = None
 
+    if "new_images_clear_key" not in ss:
+        ss.new_images_clear_key = str(uuid4())
+
     if "new_images" not in ss:
         ss.new_images = []
+
+    if "similarities" not in ss:
+        ss.similarities = []
 
 
 def main() -> None:
     st.set_page_config("Murmuration Sequential Generator")
     st.logo(ThemeConfig().get_theme_logo_path(), size="large")
+    st.header("検証用")
 
     with st.sidebar:
         ss.old_image = st.file_uploader(
             "入稿済みCRをアップロード",
             type=["png", "jpg", "mp4"],
             accept_multiple_files=False,
+            key=ss.old_image_clear_key,
             help="画像や動画ファイルを一枚アップロードすることができます。",
         )
 
@@ -78,14 +87,14 @@ def main() -> None:
                 ss.old_image = convert_video_to_image(ss.old_image)
 
             if ss.old_image.type == "image/png":
-                old_image_cols = st.columns(2)
-                with old_image_cols[0]:
-                    st.image(ss.old_image)
+                old_image_col, _, _ = st.columns(3)
+                old_image_col.image(ss.old_image)
 
         ss.new_images = st.file_uploader(
             "新規CRsをアップロード",
             type=["png", "jpg", "mp4"],
             accept_multiple_files=True,
+            key=ss.new_images_clear_key,
             help="画像や動画ファイルを複数枚アップロードすることができます。",
         )
 
@@ -94,9 +103,15 @@ def main() -> None:
                 if img.type in ["video/mp4", "video/mpeg4"]:
                     ss.new_images[i] = convert_video_to_image(img)
 
-        calculated = st.button("Calculate", type="primary")
+        clear_col, calc_col = st.columns(2)
+        cleared = clear_col.button("Clear", use_container_width=True)
+        submitted = calc_col.button("Run", type="primary", use_container_width=True)
 
-    if calculated:
+    if cleared:
+        ss.clear()
+        st.rerun()
+
+    if submitted:
         if not (ss.old_image and ss.new_images):
             st.warning("入稿済みCRと新規CRsの両方を1件以上アップロードしてください。")
             st.stop()
